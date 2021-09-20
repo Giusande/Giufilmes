bf7a5cd9fcb1821afdac0c718d1f1a8fe1f746da
Giusande <91056214+Giusande@users.noreply.github.com>
2021-09-20 17:26:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 function entrar() {
-   setTimeout(()=>{
-        window.location.href = 'https://cdpn.io/Giugiu1/debug/qBjxGKP/LQMExgDygwZk'
-    }, 3000)
+   console.log("clicou");
 }
 
 function cadastrar() {
-  setTimeout(()=>{
-        window.location.href = 'https://cdpn.io/Giugiu1/debug/powamZJ/mVAbGwxDDPmk'
-    }, 3000)
+  console.log("clicou")
 }
