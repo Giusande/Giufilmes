9ae7c74fb1d7369657ae0af3704813660eca5856
Giusande <91056214+Giusande@users.noreply.github.com>
2021-09-20 12:13:43 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function entrar(){
+   setTimeout(()=>{
+        window.location.href = 'https://cdpn.io/Giugiu1/debug/qBjxGKP/LQMExgDygwZk'
+    }, 3000)
+}
+
+function cadastrar(){
+  setTimeout(()=>{
+        window.location.href = 'https://cdpn.io/Giugiu1/debug/powamZJ/mVAbGwxDDPmk'
+    }, 3000)
+}
