9b0190ba0014f75b46a9b54e37e8a6d6f6676678
Giusande <91056214+Giusande@users.noreply.github.com>
2021-09-20 12:23:59 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
 function entrar(){
-   setTimeout(()=>{
-        window.location.href = 'https://cdpn.io/Giugiu1/debug/qBjxGKP/LQMExgDygwZk'
-    }, 3000)
+ window.location.href = 'https://cdpn.io/Giugiu1/debug/qBjxGKP/LQMExgDygwZk';
 }
 
 function cadastrar(){
-  setTimeout(()=>{
-        window.location.href = 'https://www.youtube.com/watch?v=OilQvKjpNh0&t=357s'
-    }, 3000)
+ window.location.href = 'https://www.youtube.com/watch?v=OilQvKjpNh0&t=357s';  
 }
