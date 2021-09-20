10d32f4e1774609b8f1d07963b51302e2864116d
Giusande <91056214+Giusande@users.noreply.github.com>
2021-09-20 12:20:20 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,6 @@ function entrar(){
 
 function cadastrar(){
   setTimeout(()=>{
-        window.location.href = 'https://cdpn.io/Giugiu1/debug/powamZJ/mVAbGwxDDPmk'
+        window.location.href = 'https://www.youtube.com/watch?v=OilQvKjpNh0&t=357s'
     }, 3000)
 }
